2d89d232f8e44f2b76803f94353c42201edd752f
tobibako45 <tobibako45@gmail.com>
2020-01-15 09:44:30 +0900

    Add dashboard_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#show'
+  get 'dashboard' => 'dashboard#show'
   get 'auth/auth0/callback' => 'auth0#callback'
   get 'auth/failure' => 'auth0#failure'
 end
