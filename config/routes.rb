7046dcfc0d6de57630cbeb61289737db22f6ca7b
tobibako45 <tobibako45@gmail.com>
2020-01-15 10:31:21 +0900

    Add logout_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'dashboard/show'
+  get '/' => 'home#show'
+  get '/dashboard' => 'dashboard#show'
+  get '/auth/auth0/callback' => 'auth0#callback'
+  get '/auth/failure' => 'auth0#failure'
+  get '/logout' => 'logout#logout'
   root 'home#show'
-  get 'dashboard' => 'dashboard#show'
-  get 'auth/auth0/callback' => 'auth0#callback'
-  get 'auth/failure' => 'auth0#failure'
 end
